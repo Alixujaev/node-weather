81fa5854bef8771699a252ae6821a86bee5b00fe
islom <alixujaevislom@gmail.com>
2024-06-07 20:34:13 +0500

    initial commit

---
+++ helpers/args.js
@@ -0,0 +1,21 @@
+const getArgs = (args) => {
+  const res = {}
+
+  const [executer, file, ...rest] = args
+
+  rest.forEach((value, index) => {
+    if(value.charAt(0) == '-'){
+      if(index == rest.length - 1){
+        res[value.substring(1)] = true
+      }else if(rest[index + 1].charAt(0) != '-'){
+        res[value.substring(1)] = rest[index + 1]
+      }else{
+        res[value.substring(1)] = true
+      }
+    }
+  })
+
+  return res
+}
+
+module.exports = getArgs
---
+++ index.js
@@ -0,0 +1,20 @@
+const getArgs = require('./helpers/args')
+
+const startCLI = () => {
+  const args = getArgs(process.argv);
+  console.log(args);
+
+  if(args.h){
+
+  }
+
+  if(args.s){
+
+  }
+
+  if(args.t){
+    
+  }
+}
+
+startCLI()
